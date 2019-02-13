12256047f7606b3835899ccda49b0be917a27e73
Fangdun Cai <cfddream@gmail.com>
2019-02-13 13:24:49 +0800

    feat: add node api

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub trait Radix<T> {
     fn insert(&mut self, path: &str, data: T);
     fn find(&self, path: &str) -> Option<&Node<T>>;
     fn remove(&mut self, path: &str);
+    fn add_node(&mut self, path: &str, data: T) -> &mut Node<T>;
 }
 
 #[derive(Debug)]
@@ -25,13 +26,13 @@ impl<T> Node<T> {
     }
 
     // Insert with `&mut Vec<u8>` path and `Option<T>` data
-    pub fn insert_with(&mut self, path: &mut Vec<u8>, data: Option<T>) {
+    pub fn insert_with(&mut self, path: &mut Vec<u8>, data: Option<T>) -> &mut Node<T> {
         // empty node
         let sl = self.path.len();
         if (sl | self.indices.len()) == 0 {
             self.data = data;
             self.path = path.to_owned();
-            return;
+            return self;
         }
 
         let pl = path.len();
@@ -63,28 +64,12 @@ impl<T> Node<T> {
         // "abc" == "abc"
         if i == pl {
             self.data = data;
-            return;
+            return self;
         }
 
         // "abcde" > "abc"
         // New Node
-        let l = self.indices.len();
-        let c = path[i];
-        let mut j = 0;
-        while j < l {
-            if c == self.indices[j] {
-                return self.nodes[j].insert_with(&mut path.split_off(i), data);
-            }
-            j += 1;
-        }
-
-        self.indices.push(c);
-        self.nodes.push(Node {
-            data,
-            nodes: Vec::new(),
-            indices: Vec::new(),
-            path: path.split_off(i),
-        });
+        self.add_node_with(path, data, i)
     }
 
     // Find with `&mut Vec<u8>` path
@@ -123,15 +108,41 @@ impl<T> Node<T> {
         // not found
         None
     }
+
+    pub fn add_node_with(&mut self, path: &mut Vec<u8>, data: Option<T>, i: usize) -> &mut Node<T> {
+        let l = self.indices.len();
+        let c = path[i];
+        let mut j = 0;
+        while j < l {
+            if c == self.indices[j] {
+                return self.nodes[j].insert_with(&mut path.split_off(i), data);
+            }
+            j += 1;
+        }
+
+        self.indices.push(c);
+        self.nodes.push(Node {
+            data,
+            nodes: Vec::new(),
+            indices: Vec::new(),
+            path: path.split_off(i),
+        });
+
+        &mut self.nodes[l]
+    }
 }
 
 impl<T> Radix<T> for Node<T> {
     fn insert(&mut self, path: &str, data: T) {
-        self.insert_with(&mut path.as_bytes().to_owned(), Some(data))
+        self.insert_with(&mut path.as_bytes().to_owned(), Some(data));
     }
 
     fn find(&self, path: &str) -> Option<&Node<T>> {
         self.find_with(&mut path.as_bytes().to_owned())
+    }
+
+    fn add_node(&mut self, path: &str, data: T) -> &mut Node<T> {
+        self.add_node_with(&mut path.as_bytes().to_owned(), Some(data), 0)
     }
 
     #[allow(unused_variables)]
@@ -282,6 +293,52 @@ mod tests {
         assert_eq!(node.is_some(), true);
 
         let node = tree.find("你好，世界 Rust");
+        assert_eq!(node.is_some(), false);
+    }
+
+    #[test]
+    fn insert_then_return_new_node() {
+        let mut tree = Node::<u8>::new("", b' ');
+
+        let a = tree.add_node("a", b'a');
+        let b = a.add_node("b", b'b');
+        let c = b.add_node("c", b'c');
+        let d = c.add_node("d", b'd');
+        let e = d.add_node("e", b'e');
+        e.print_nodes(0);
+        d.print_nodes(0);
+        c.print_nodes(0);
+        b.print_nodes(0);
+        a.print_nodes(0);
+
+        tree.print_nodes(0);
+
+        println!("{:#?}", tree);
+
+        let node = tree.find("a");
+        assert_eq!(node.is_some(), true);
+        assert_eq!(node.unwrap().data.unwrap(), b'a');
+
+        let node = node.unwrap().find("b");
+        assert_eq!(node.is_some(), true);
+        assert_eq!(node.unwrap().data.unwrap(), b'b');
+
+        let node = node.unwrap().find("c");
+        assert_eq!(node.is_some(), true);
+        assert_eq!(node.unwrap().data.unwrap(), b'c');
+
+        let node = node.unwrap().find("d");
+        assert_eq!(node.is_some(), true);
+        assert_eq!(node.unwrap().data.unwrap(), b'd');
+
+        let node = node.unwrap().find("e");
+        assert_eq!(node.is_some(), true);
+        assert_eq!(node.unwrap().data.unwrap(), b'e');
+
+        let node = tree.find("abcdef");
+        assert_eq!(node.is_some(), false);
+
+        let node = tree.find("b");
         assert_eq!(node.is_some(), false);
     }
 }
